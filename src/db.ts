040de60ded41855f5c089cbf1a9ff68c91e6aee5
Karl J. Overå <k@rl.run>
2024-02-03 12:13:17 +0100

    chore: apply prettier

--- src/db.ts
+++ src/db.ts
@@ -1,66 +1,60 @@
-import { Client } from "pg";
-import * as path from "path";
-import { cwd } from "node:process";
+import { Client } from 'pg'
+import * as path from 'path'
+import { cwd } from 'node:process'
 
-const host = Bun.env.DOCKER_COMPOSE ? "db" : "localhost";
+const host = Bun.env.DOCKER_COMPOSE ? 'db' : 'localhost'
 
 export const client = new Client({
   host: host,
   port: 5432,
-  database: "postgres",
-  user: "postgres",
-  password: "postgres",
-});
+  database: 'postgres',
+  user: 'postgres',
+  password: 'postgres',
+})
 
 export async function initDb() {
-  console.info("Running migrations if needed");
+  console.info('Running migrations if needed')
 
-  await connectWithRetry();
+  await connectWithRetry()
 
   const authorsExistQuery = await client.query(`
         SELECT EXISTS (SELECT
                        FROM information_schema.tables
                        WHERE table_schema = 'public'
                          AND table_name = 'authors');
-    `);
+    `)
 
   if (!authorsExistQuery.rows[0].exists) {
-    console.info("Schema doesn't exist, running initial schema.sql");
+    console.info("Schema doesn't exist, running initial schema.sql")
 
     try {
-      await client.query("BEGIN");
-      await client.query(
-        await Bun.file(path.join(cwd(), "db", "schema.sql")).text(),
-      );
-      await client.query(
-        await Bun.file(path.join(cwd(), "db", "seed.sql")).text(),
-      );
-      await client.query("COMMIT");
+      await client.query('BEGIN')
+      await client.query(await Bun.file(path.join(cwd(), 'db', 'schema.sql')).text())
+      await client.query(await Bun.file(path.join(cwd(), 'db', 'seed.sql')).text())
+      await client.query('COMMIT')
 
-      console.info("Created schema and seeded database");
+      console.info('Created schema and seeded database')
     } catch (e) {
-      console.error(new Error("Unable to run migrations", { cause: e }));
-      await client.query("ROLLBACK");
+      console.error(new Error('Unable to run migrations', { cause: e }))
+      await client.query('ROLLBACK')
     }
   } else {
-    console.info("Schema exists, skipping migrations");
+    console.info('Schema exists, skipping migrations')
   }
 }
 
 async function connectWithRetry() {
-  let retry = 0;
+  let retry = 0
   while (true) {
     try {
-      await client.connect();
-      break;
+      await client.connect()
+      break
     } catch (e) {
-      retry++;
+      retry++
 
-      if (retry > 3) throw e;
-      console.error(
-        `Unable to connect to database, retrying in 3 second (${retry}/3)`,
-      );
-      await new Promise((resolve) => setTimeout(resolve, 3000));
+      if (retry > 3) throw e
+      console.error(`Unable to connect to database, retrying in 3 second (${retry}/3)`)
+      await new Promise((resolve) => setTimeout(resolve, 3000))
     }
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,31 @@
-import { Elysia, t } from "elysia";
-import { client, initDb } from "./db.ts";
-import swagger from "@elysiajs/swagger";
+import { Elysia, t } from 'elysia'
+import { client, initDb } from './db.ts'
+import swagger from '@elysiajs/swagger'
 
-await initDb();
+await initDb()
 
 const app = new Elysia()
   .use(
     swagger({
       documentation: {
         info: {
-          title: "Fake Bookshop",
-          description: "For fg-frontendrammeverk 2024",
-          version: "1.0.0",
+          title: 'Fake Bookshop',
+          description: 'For fg-frontendrammeverk 2024',
+          version: '1.0.0',
         },
       },
     }),
   )
   .get(
-    "authors",
+    'authors',
     async () => {
-      const result = await client.query("SELECT * FROM authors");
+      const result = await client.query('SELECT * FROM authors')
 
       return result.rows.map((it) => ({
         author_id: it.author_id,
         name: it.name,
         bio: it.bio,
-      }));
+      }))
     },
     {
       response: t.Array(
@@ -34,28 +34,25 @@ const app = new Elysia()
           name: t.String(),
           bio: t.String(),
         }),
-        { description: "Get all authors" },
+        { description: 'Get all authors' },
       ),
     },
   )
   .get(
-    "author/:id",
+    'author/:id',
     async ({ params: { id } }) => {
-      const result = await client.query(
-        "SELECT * FROM authors WHERE author_id = $1",
-        [id],
-      );
+      const result = await client.query('SELECT * FROM authors WHERE author_id = $1', [id])
 
       if (result.rows.length === 0) {
-        return null;
+        return null
       }
 
-      const author = result.rows[0];
+      const author = result.rows[0]
       return {
         author_id: author.author_id,
         name: author.name,
         bio: author.bio,
-      };
+      }
     },
     {
       response: t.Nullable(
@@ -65,13 +62,13 @@ const app = new Elysia()
             name: t.String(),
             bio: t.String(),
           },
-          { description: "Get author by UUID" },
+          { description: 'Get author by UUID' },
         ),
       ),
     },
   )
   .get(
-    "author/:id/books",
+    'author/:id/books',
     async ({ params: { id } }) => {
       const result = await client.query(
         `SELECT *
@@ -79,88 +76,83 @@ const app = new Elysia()
                           LEFT JOIN authors a on a.id = books.author_id
                  WHERE a.author_id = $1`,
         [id],
-      );
+      )
 
       return result.rows.map((it) => ({
         title: it.title,
         published_date: it.published_date.toISOString().slice(0, 10),
         isbn: it.isbn,
-      }));
+      }))
     },
     {
       response: t.Array(
         t.Object({
           title: t.String(),
-          published_date: t.String({ description: "ISO8601 Date" }),
+          published_date: t.String({ description: 'ISO8601 Date' }),
           isbn: t.String(),
         }),
-        { description: "Get all books for author by UUID" },
+        { description: 'Get all books for author by UUID' },
       ),
     },
   )
   .get(
-    "books",
+    'books',
     async () => {
-      const result = await client.query("SELECT * FROM books");
+      const result = await client.query('SELECT * FROM books')
 
       return result.rows.map((it) => ({
         title: it.title,
         published_date: it.published_date.toISOString().slice(0, 10),
         isbn: it.isbn,
-      }));
+      }))
     },
     {
       response: t.Array(
         t.Object({
           title: t.String(),
-          published_date: t.String({ description: "ISO8601 Date" }),
+          published_date: t.String({ description: 'ISO8601 Date' }),
           isbn: t.String(),
         }),
-        { description: "Get all books" },
+        { description: 'Get all books' },
       ),
     },
   )
   .put(
-    "book",
+    'book',
     async ({ body, set }) => {
       // if isbn already exists
-      if (
-        await client
-          .query("SELECT * FROM books WHERE isbn = $1", [body.isbn])
-          .then((it) => it.rows.length > 0)
-      ) {
-        set.status = "Conflict";
-        return;
+      if (await client.query('SELECT * FROM books WHERE isbn = $1', [body.isbn]).then((it) => it.rows.length > 0)) {
+        set.status = 'Conflict'
+        return
       }
-      const author = await client.query(
-        "SELECT * FROM authors WHERE author_id = $1",
-        [body.author_id],
-      );
-      await client.query(
-        "INSERT INTO books (isbn, title, published_date, author_id) VALUES ($1, $2, $3, $4)",
-        [body.isbn, body.title, body.published_date, author.rows[0].id],
-      );
+      const author = await client.query('SELECT * FROM authors WHERE author_id = $1', [body.author_id])
+      await client.query('INSERT INTO books (isbn, title, published_date, author_id) VALUES ($1, $2, $3, $4)', [
+        body.isbn,
+        body.title,
+        body.published_date,
+        author.rows[0].id,
+      ])
     },
     {
       body: t.Object({
         isbn: t.String(),
         title: t.String(),
-        published_date: t.String({ description: "ISO8601 Date" }),
-        author_id: t.String({ description: "UUID of author" }),
+        published_date: t.String({ description: 'ISO8601 Date' }),
+        author_id: t.String({ description: 'UUID of author' }),
       }),
     },
   )
   .onError((err) => {
-    console.error(err);
-  });
+    console.error(err)
+  })
 
 app.onStop(async () => {
-  console.log("Stopping server, closing database connection");
-  await client.end();
-});
+  console.log('Stopping server, closing database connection')
+  await client.end()
+})
 
-app.listen(6969);
+app.listen(6969)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}, go to http://${app.server?.hostname}:${app.server?.port}/swagger for swagger docs.`,
-);
+)
